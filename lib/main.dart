df0e29d01ad88a4339c063daf6e92bfbebb8f706
kimkyuhun <yj302kim@naver.com>
2023-10-05 00:37:48 +0900

    매세지 잖아

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,183 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+      routes: {
+        // routes 설정 생략
+      },
+    );
+  }
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = [
+    CommunityScreen(),
+    CalendarScreen(),
+    MatchingScreen(),
+    UserScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Health App'),
+      ),
+      body: _screens[_currentIndex],
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Text('Menu'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Community'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/community');
+              },
+            ),
+            ListTile(
+              title: Text('Calendar'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/calendar');
+              },
+            ),
+            ListTile(
+              title: Text('Matching'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/matching');
+              },
+            ),
+            ListTile(
+              title: Text('User'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/user');
+              },
+            ),
+            ListTile(
+              title: Text('Login'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/login');
+              },
+            ),
+          ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'Community',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'Matching',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'User',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CommunityScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Community'),
+      ),
+      body: Center(
+        child: Text('Community Screen'),
+      ),
+    );
+  }
+}
+
+class CalendarScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calendar'),
+      ),
+      body: Center(
+        child: Text('Calendar Screen'),
+      ),
+    );
+  }
+}
+
+class MatchingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Matching'),
+      ),
+      body: Center(
+        child: Text('Matching Screen'),
+      ),
+    );
+  }
+}
+
+class UserScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User'),
+      ),
+      body: Center(
+        child: Text('User Screen'),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: Text('This is the Login Screen'),
       ),
     );
   }
